33c20b7880c3ce1805ef060a6e9e55d84f7bf6c6
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-07-18 18:01:30 +0300

    done

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -9,15 +9,15 @@ app = Flask(  # Create a flask app
 )
 
 # Variables for tasks
-image_link = "https://i.imgur.com/o9Znt5k.png"
+image_link = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
 
 user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
 
 posts = {
-    "https://i.imgur.com/1dSgGnG.png": "The cohort of 2022!",
-    "https://i.imgur.com/CPEvMk0.jpg": "MEET graduation!",
-    "https://i.imgur.com/Cb7LK9o.jpg": "#MEET_HACKATHON",
-    "https://i.imgur.com/S5A93Wt.jpg": "Class of 2024's Y1 closing event cohort"}
+    "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg": "The cohort of 2022!",
+    "https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg": "MEET graduation!",
+    "https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000": "#MEET_HACKATHON",
+    "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg": "Class of 2024's Y1 closing event cohort"}
 
 
 #####
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',image_link = image_link,user_bio = user_bio,posts = posts)
 
 
 @app.route('/about')  # '/' for the default page
